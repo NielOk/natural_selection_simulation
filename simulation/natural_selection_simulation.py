b7fd86a4afc0b73ad070a5e0810af1725faeef0a
Niel Ok <nielok@stanford.edu>
2024-11-25 17:11:15 -0800

    added loading of organism parameters

--- simulation/natural_selection_simulation.py
+++ simulation/natural_selection_simulation.py
@@ -11,6 +11,7 @@ import json
 
 PROJECT_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ENVIRONMENTS_DIR = os.path.join(PROJECT_BASE_DIR, 'environments')
+ORGANISM_CONFIGS_DIR = os.path.join(PROJECT_BASE_DIR, 'organism-configs')
 
 # Unchanging simulation parameters
 food_opportunities = 3
@@ -27,3 +28,19 @@ initial_food = environment_def_dict["initial_food"] # Initial amount of food ava
 days_to_regenerate_food = environment_def_dict["days_to_regenerate_food"] # Number of days it takes to regenerate food after being eaten.
 area = environment_def_dict["area"] # Numerical representation of amount of space available in the environment
 
+# Organism parameters
+organism_configs_to_use = input("Input the filename for the organism configs you would like to use (e.g. organism_configs_1.json): ")
+organism_config_path = os.path.join(ORGANISM_CONFIGS_DIR, organism_configs_to_use)
+
+with open(organism_config_path, 'r') as json_file:
+    organism_config_dict = json.load(json_file)
+
+initial_energy = organism_config_dict["initial_energy"]
+required_energy = organism_config_dict["required_energy"]
+initial_speed = organism_config_dict["initial_speed"]
+initial_size = organism_config_dict["initial_size"]
+initial_sense = organism_config_dict["initial_sense"]
+hunt_energy = organism_config_dict["hunt_energy"]
+run_energy = organism_config_dict["run_energy"]
+
+# Simulation parameters
